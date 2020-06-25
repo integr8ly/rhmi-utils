6eb3747ee9cf2b2a9f966114d154404b78c13854
jessesarn <jsarnovsky@redhat.com>
2020-06-25 09:28:24 -0400

    fix ocp 3.x compatibility

--- support/get-resources.sh
+++ support/get-resources.sh
@@ -81,7 +81,7 @@ cat << EOF > ${work_file}
     },
     {
       "id": "enmasse-crs",
-      "cmd": "oc get $(echo $(oc api-resources | grep enmasse | awk '{print $1}') | sed 's/ /,/g') --all-namespaces -o json | jq 'reduce inputs as \$i (.; .items += \$i.items)'",
+      "cmd": "(oc get $(echo $(oc api-resources | grep enmasse | awk '{print $1}' | egrep -v '^addressspaceschemas$') | sed 's/ /,/g') --all-namespaces  -o json; oc get addressspaceschemas -o json) | jq 'reduce inputs as \$i (.; .items += \$i.items)'",
       "type": "json"
     },
     {
